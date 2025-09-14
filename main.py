3935eee0840dc5c2cf99567f64fcb0b99c5b8353
Cuong Le <cuonglh2807@gmail.com>
2025-09-14 21:26:47 +0700

    set debug mode

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from langchain.chains import ConversationChain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
 from langchain_openai import ChatOpenAI
+from langchain.globals import set_debug
 
 from prompt_config import MASTER_PROMPT
 from rag_util import RAG
@@ -12,6 +13,7 @@ if not os.environ.get("OPENAI_API_KEY"):
   os.environ["OPENAI_API_KEY"] = "sk-abc-xyz"
 
 llm = ChatOpenAI(model="gpt-4o")
+set_debug(True)
 question_answer_chain = create_stuff_documents_chain(llm, MASTER_PROMPT)
 rag_chain = create_retrieval_chain(RAG.get_vector_store_retriever(), question_answer_chain)
 
